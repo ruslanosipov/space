308ec49c3ae1c61a99cd926162a601216ae874c1
Ruslan Osipov <ruslan@rosipov.com>
2013-06-28 20:00:22 -0400

    Server handles client disconnects without crashes
    * Server tries to resend data for 10 seconds if client is not
    accepting connection.

--- lib/server.py
+++ lib/server.py
@@ -18,6 +18,8 @@ class Server:
         self.out = []
         self.err = []
         self.data = {}
+        self.attempts = {}
+        self.timeout = 0.02  # seconds
 
     def listen(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,23 +30,36 @@ class Server:
 
     def receive(self):
         self.data = {k: {} for k in self.data.keys()}  # clear data values
-        timeout = 0.02  # seconds
-        inp, out, err = select.select(self.inp, self.out, self.err, timeout)
+        inp, out, err = select.select(self.inp, self.out,
+                                      self.err, self.timeout)
         for s in inp:
             if s == self.server:
                 client, address = self.server.accept()
                 self.inp.append(client)
             else:
-                data = s.recv(self.size)
-                if data:
-                    self.data[s] = net.net_eval(data)
-                else:
-                    s.close()
-                    self.inp.remove(s)
+                try:
+                    data = s.recv(self.size)
+                    if data:
+                        self.data[s] = net.net_eval(data)
+                except socket.error:
+                    # Do nothing if can not receive, see send()
+                    pass
 
     def send(self):
         for s in self.data.keys():
-            s.send(net.net_repr(self.data[s]))
+            try:
+                s.send(net.net_repr(self.data[s]))
+            except socket.error:
+                if s not in self.attempts:
+                    print "Client cowardly disconnected, retrying..."
+                    self.attempts[s] = 1
+                else:
+                    self.attempts[s] += 1
+                    if self.attempts[s] > (1.0 / self.timeout) * 10:
+                        print "Client cowardly disconnected, stopping retries."
+                        del self.data[s]
+                        s.close()
+                        self.inp.remove(s)
 
     def close(self):
         self.server.close()
